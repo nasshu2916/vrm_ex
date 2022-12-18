9fb8c417c1422378656d45a5c91ce056b6324284
NaoyaKohda <naoya.kohda@gmail.com>
2022-12-18 18:38:47 +0900

    mix new vrm_ex

---
+++ lib/vrm_ex.ex
@@ -0,0 +1,18 @@
+defmodule VrmEx do
+  @moduledoc """
+  Documentation for `VrmEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VrmEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vrm_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule VrmExTest do
+  use ExUnit.Case
+  doctest VrmEx
+
+  test "greets the world" do
+    assert VrmEx.hello() == :world
+  end
+end
